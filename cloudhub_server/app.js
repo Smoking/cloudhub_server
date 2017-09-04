982a85b74194261b5fee414382bea88a3c0f0e6f
Smoking <287482894@qq.com>
2017-09-04 23:23:02 +0800

    调整显示模板

--- cloudhub_server/app.js
+++ cloudhub_server/app.js
@@ -1,30 +1,56 @@
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var swig = require('swig');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var debug = require('debug')('blog:server');
+var http = require('http');
+
+
 var app = express();
+//设置端口号
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+function normalizePort(val) {
+    var port = parseInt(val, 10);
 
-// view engine setup
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+
+// view engine setup  模板设定
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
+app.engine('html',swig.renderFile);
+app.set('view engine', 'html');
+swig.setDefaults({
+    cache : false //不缓存文件
+});//在开发过程中取消模板缓存
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'release')));
+//设置路由
 app.use('/', index);
 app.use('/users', users);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -43,4 +69,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-module.exports = app;
+
+var server = app.listen(3000, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('app listening ', host, port);
+});
+
+
